c4d608f5b2cd511ab842340fc9949614cb110896
Chandrasekhar77777 <chandramururi43@gmail.com>
2025-10-29 22:47:25 +0530

    Initial commit

---
+++ new.py
@@ -0,0 +1,44 @@
+n = int ( input("enter a number:"))
+print (n)
+
+s = float ( input("Enter a number:"))
+print (s)
+
+m = str ( input("Enter a name:"))  
+n = ( input("Enter a name:"))
+print (m)
+
+l = input("What is your name:")
+k = ("hello"+ l)
+print(k)
+
+l = input("how old are you:")
+k = ("i am " +l+ "years old")
+print(k)
+
+name = "hansika"
+cls = "motoni"
+rollno = "remo"
+
+
+
+print(name)
+print(cls)
+print(rollno)
+
+x = "27"
+print(x)
+print(type(x))
+
+s=2
+d=4.8
+e="5"
+a=True
+print(s)
+print(d)
+print(e)
+print(a)
+print(type(s))
+print(type(d))
+print(type(e))
+print(type(a))
